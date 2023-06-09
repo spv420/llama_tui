3f6eed54ee22c5e3cfe5bd1e7574da3330bc18fb
spv420 <spv@spv.sh>
2023-06-09 11:50:49 -0400

    shit

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from llama_cpp import Llama
 import json
 
-llm = Llama(model_path="/home/spv/llama.cpp/ggml-vicuna13b-q2_k.bin", verbose=False)
+llm = Llama(model_path="/home/spv/llama.cpp/ggml-vicuna13b-q2_k.bin", verbose=False, n_threads=4)
 
-output = llm("### Human: Hi there, hello\n### Assistant:", max_tokens=32, stop=["### Human:"], echo=False, stream=True)
+history = ""
 
-for tok in output:
-	print(tok["choices"][0]["text"], flush=True)
+def send_message(s):
+	global history
+	history += "### Human: " + s + "\n### Assistant:"
+
+	output = llm(history, max_tokens=256, stop=["### Human:"], echo=False, stream=True)
+
+	for tok in output:
+		yield tok["choices"][0]["text"]
+
+	history += "\n"
+
+while True:
+	print(history)
+	s = input("> ")
+	for tok in send_message(s):
+		history += tok
+		print(tok, end="", flush=True)
+	print()
