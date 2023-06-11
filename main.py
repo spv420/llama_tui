bb25ead7a92cbda2cc09b96b1955fcb08f1ff773
spv420 <spv@spv.sh>
2023-06-10 23:16:23 -0400

    code

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from llama_cpp import Llama
+from rich import print
 import json
 import sys
 
-llm = Llama(model_path="/home/spv/llama.cpp/ggml-vicuna13b-q2_k.bin", verbose=False, n_threads=4)
+llm = Llama(model_path="/home/spv/llama_tui/wizardvicuna-7b-q2_K.bin", verbose=False, n_threads=4)
 
 history = ""
 
@@ -19,8 +20,11 @@ def send_message(s):
 
 while True:
 	s = input("> ")
+	m = ""
 	print("@", end="", flush=True)
 	for tok in send_message(s):
 		history += tok
+		m += tok
 		print(tok, end="", flush=True)
+#	print(m, flush=True)
 	print()
